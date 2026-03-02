e7401a5ae9620df9045ca51cd9975abfc1a251d5
antwanchild <113145842+antwanchild@users.noreply.github.com>
2026-03-01 21:48:16 -0700

    Improve PUID/PGID logging in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,17 @@
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
-groupmod -o -g "$PGID" botgroup
-usermod -o -u "$PUID" botuser
+if [ "$(id -u botuser)" != "$PUID" ]; then
+    usermod -u "$PUID" botuser
+else
+    echo "PUID already set to $PUID — no changes needed"
+fi
+
+if [ "$(id -g botgroup)" != "$PGID" ]; then
+    groupmod -g "$PGID" botgroup
+else
+    echo "PGID already set to $PGID — no changes needed"
+fi
 
 chown -R botuser:botgroup /app
 
